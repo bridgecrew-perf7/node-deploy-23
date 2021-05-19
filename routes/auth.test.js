a85017d242c8e55750d5400a96b8bbc9b9196347
gerald119 <goodman2678@gmail.com>
2021-05-20 00:13:19 +0900

    Initial commit

---
+++ routes/auth.test.js
@@ -0,0 +1,54 @@
+const require = require('supertest');
+const { sequelize } = require('../models');
+const app = require('../app');
+const { beforeFindAfterExpandIncludeAll } = require('../models/user');
+
+
+beforeAll(async () => {
+    await sequelize.sync();
+});
+
+describe('POST / join', () => {
+    test('로그인 안했으면 가입', (done) => {
+        request(app)
+        .post('/auth/join')
+        .send({
+            email: 'zeroch0@gmail.com',
+            nick: 'zerocho',
+            password: 'nodejsbook',
+        })
+        .expect('Location', '/')
+        .expect(302, done);
+    });
+});
+
+describe('POST /join', () => {
+    const agent = request.agent(app);
+    beforeEach((done) => {
+        agent
+            .post('/auth/login')
+            .send({
+                email: 'zerohch0@gmail.com',
+                password: 'nodejsbook',
+            })
+            .end(done);
+    });
+
+    test('이미 로그인했으면 redirect/', (done) => {
+        const message = encodeURIComponent('로그인한 상태입니다');
+        agent
+        .post('/auth/join')
+        .send({
+            email: 'zerohch0@gmail.com',
+            nick: 'zerocho',
+            password:'nodejsbook',
+        })
+        .expect('Location', `/?error=${message}`)
+        .expect(302, done);
+    });
+});
+
+afterAll(async () => {
+    await sequelize.sync({ force: true });
+});
+
